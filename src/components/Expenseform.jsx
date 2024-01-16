a0c28f0201db6333d5bf7a4d690c3bba9b93933a
shmas khan <sktanoli420@gmail.com>
2024-01-17 00:50:27 +0500

    implement form validation

--- src/components/Expenseform.jsx
+++ src/components/Expenseform.jsx
@@ -1,45 +1,72 @@
 import React, { useState } from "react";
 
 export const Expenseform = ({ setexpenses }) => {
+  const [formvalue, setformvalue] = useState({
+    title: "",
+    category: "",
+    amount: "",
+  });
 
-  const [formvalue,setformvalue ] =useState({
-    title:'',
-    category:'',
-    amount:'',
-  })
+  const [error, setError] = useState({});
 
+  const validate = (formdata) => {
+    const errorData = {};
+    if (!formdata.title) {
+      errorData.title = "title is required";
+    }
+    if (!formdata.category) {
+      errorData.category = "category is required";
+    }
+    if (!formdata.amount) {
+      errorData.amount = "amount is required";
+    }
+  
+    setError(errorData);
+  };
   const handlesubmit = (e) => {
     e.preventDefault();
-    // setexpenses((prevsate)=>[...prevsate,{...getFormData(e.target),id:self.crypto.randomUUID()}])
-    // e.target.reset()
-    setexpenses(prevstate=>[...prevstate,{...formvalue, id:self.crypto.randomUUID()}])
+    const validateResult = validate(formvalue); // Corrected from validate(expense)
+    if (Object.keys(validateResult).length) return;
+
+    setexpenses((prevstate) => [
+      ...prevstate,
+      { ...formvalue, id: self.crypto.randomUUID() },
+    ]);
+
     setformvalue({
-      title: '',
-      category: '',
-      amount: '',
+      title: "",
+      category: "",
+      amount: "",
     });
   };
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformvalue((prevstate) => ({ ...prevstate, [name]: value }));
+    setError((prevErrors) => ({ ...prevErrors, [name]: "" }));
+  };
 
-  // const getFormData = (form) => {
-  //   const formDate = new FormData(form);
-  //   const data = {};
-  //   for (const [key, value] of formDate.entries()) {
-  //     data[key] = value;
-  //   }
-  //   return data
-  // }
   return (
     <>
       <form className="expense-form" onSubmit={handlesubmit}>
         <div className="input-container">
           <label htmlFor="title">Title</label>
-          <input id="title" name="title" value={formvalue.title} onChange={e=>setformvalue(prevstate=>({...prevstate,title:e.target.value}))} />
+          <input
+            id="title"
+            name="title"
+            value={formvalue.title}
+            onChange={handleChange}
+          />
+          <p className="error">{error.title}</p>
         </div>
         <div className="input-container">
           <label htmlFor="category">Category</label>
-
-          <select id="category" name="category" value={formvalue.category} onChange={e=>setformvalue(prevstate=>({...prevstate,category:e.target.value}))}>
+          <select
+            id="category"
+            name="category"
+            value={formvalue.category}
+            onChange={handleChange}
+          >
             <option value="" hidden>
               Select Category
             </option>
@@ -49,10 +76,17 @@ export const Expenseform = ({ setexpenses }) => {
             <option value="education">Education</option>
             <option value="medicine">Medicine</option>
           </select>
+          <p className="error">{error.category}</p>
         </div>
         <div className="input-container">
           <label htmlFor="amount">Amount</label>
-          <input id="amount" name="amount" value={formvalue.amount} onChange={e=>setformvalue(prevstate=>({...prevstate,amount:e.target.value}))}/>
+          <input
+            id="amount"
+            name="amount"
+            value={formvalue.amount}
+            onChange={handleChange}
+          />
+          <p className="error">{error.amount}</p>
         </div>
         <button className="add-btn">Add</button>
       </form>
