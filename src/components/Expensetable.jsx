6e631d8b82f2343d27f02940d97c6c13cc0be1fd
shmas khan <sktanoli420@gmail.com>
2024-01-20 17:19:27 +0500

    add sorting

--- src/components/Expensetable.jsx
+++ src/components/Expensetable.jsx
@@ -64,6 +64,7 @@ export const Expensetable = ({ expenses, setexpenses,setformvalue,setEditingRowI
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={()=>setexpenses(prevState=>[...prevState.sort((a,b)=>a.amount - b.amount)])}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -73,6 +74,7 @@ export const Expensetable = ({ expenses, setexpenses,setformvalue,setEditingRowI
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={()=>setexpenses(prevState=>[...prevState.sort((a,b)=>b.amount - a.amount)])}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -91,7 +93,7 @@ export const Expensetable = ({ expenses, setexpenses,setformvalue,setEditingRowI
           ))}
           <tr>
             <th>Total</th>
-            <th></th>
+            <th className="clear-sort"> Clear sort</th>
             <th>Rs: {total}</th>
           </tr>
         </tbody>
